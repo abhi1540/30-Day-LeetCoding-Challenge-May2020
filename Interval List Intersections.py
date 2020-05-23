934ae4f723e0eef93f64af178b9a444c26a849d5
abhi1540 <abhisek1651990@gmail.com>
2020-05-23 22:54:28 +0530

    Add files via upload

---
+++ Interval List Intersections.py
@@ -0,0 +1,19 @@
+def intervalIntersection(self, A: List[List[int]], B: List[List[int]]) -> List[List[int]]:
+        
+        
+        res = []
+        i,j = 0,0
+        
+        if A and B:
+            while i < len(A) and j < len(B):
+                low = max(A[i][0], B[j][0])
+                high = min(A[i][1], B[j][1])
+                
+                if low <= high:
+                    res.append([low,high])
+                    
+                if A[i][1] < B[j][1]:
+                    i += 1
+                else:
+                    j += 1
+            return res
